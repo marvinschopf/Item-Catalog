a6e712e10fc239925fcf34c51b29d06507d614d9
MagicMarvMan <24806960+MagicMarvMan@users.noreply.github.com>
2018-01-01 21:06:52 +0100

    Added database models

--- database_setup.py
+++ database_setup.py
@@ -15,13 +15,30 @@ class User(Base):
     picture = Column(String(250))
 
 
-class Restaurant(Base):
-    __tablename__ = 'restaurant'
+class Category(Base):
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    description = Column(String(250), nullable=False)
+
+    @property
+    def serialize(self):
+        return {
+            'name': self.name,
+            'description': self.description,
+            'id': self.id
+        }
+
+class Item(Base):
+    __tablename__ = 'item'
 
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User)
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category)
 
     @property
     def serialize(self):
@@ -29,35 +46,13 @@ class Restaurant(Base):
         return {
             'name': self.name,
             'id': self.id,
+            'user_id': self.user_id,
+            'category_id': self.category_id
         }
 
 
-class MenuItem(Base):
-    __tablename__ = 'menu_item'
 
-    name = Column(String(80), nullable=False)
-    id = Column(Integer, primary_key=True)
-    description = Column(String(250))
-    price = Column(String(8))
-    course = Column(String(250))
-    restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
-    restaurant = relationship(Restaurant)
-    user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(User)
-
-    @property
-    def serialize(self):
-        """Return object data in easily serializeable format"""
-        return {
-            'name': self.name,
-            'description': self.description,
-            'id': self.id,
-            'price': self.price,
-            'course': self.course,
-        }
-
-
-engine = create_engine('sqlite:///restaurantmenuwithusers.db')
+engine = create_engine('sqlite:///itemcatalog.db')
 
 
 Base.metadata.create_all(engine)
