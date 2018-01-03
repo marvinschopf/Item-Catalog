6b848f4d2504e83842c34b23732caa1cfe8f9fb3
MagicMarvMan <24806960+MagicMarvMan@users.noreply.github.com>
2018-01-03 14:53:08 +0100

    Disable commit

--- app.py
+++ app.py
@@ -243,7 +243,7 @@ def githubAuthorized():
     login_session["token"] = (resp['access_token'], '')
     me = github.get('user')
     login_session["provider"] = "github"
-    login_session["email"] = "github-"+me.data["username"]+"@users.item-catalog"
+    login_session["email"] = "github-"+""+"@users.item-catalog"
     #if(me.data["email"] is None):
         #login_session["email"] = ""
     #else:
